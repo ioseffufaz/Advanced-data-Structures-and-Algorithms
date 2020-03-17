0e1c109689adb86777223bf462ca9b52542dbd52
joseph <yusif.ibrahimov@ufaz.az>
2020-03-17 21:14:09 +0100

    Searching and Sorting algosithms

--- Searching_Sorting/bubble_sort.c
+++ Searching_Sorting/bubble_sort.c
@@ -20,6 +20,15 @@ void bubble_sort(int array[],int size){
 	}
 }
 
+/*
+Analysis Of Bubble Sort
+ In Bubble Sort, n-1 comparisons will be done in the 1st pass, n-2 in 2nd pass, n-3 in 3rd pass and so on. So the total number of comparisons will be,
+(n-1) + (n-2) + (n-3) + ..... + 3 + 2 + 1
+Sum =          n(n-1)/2
+Complexity:    O(n2)
+ 
+
+*/
 int main(void){
 	int array[] = {1,2,7,5,9,3,15,11,17,12};
 	bubble_sort(array,10);
